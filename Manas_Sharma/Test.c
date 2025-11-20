b9ad950d734df071171ea615f8f46d0ecff523f0
Manas Sharma <manasmailsss@gmail.com>
2025-11-20 11:52:00 +0530

    Really Enjoyed the open sourcery sessions

--- Manas_Sharma/Test.c
+++ Manas_Sharma/Test.c
@@ -3,7 +3,7 @@ int main(){
     printf("Hello Team Scimat\n");
     printf("Really appreciate your efforts towards to Open Sourcery\n");
     print("I already had a little bit of knowledge of Github but Your sessions made me better");
-    printf("Congrats on the success of the events and sessions!!");
+    printf("Congrats on the success of the events and sessions  !!");
     return 1; //cause you are the one 
 
 }
